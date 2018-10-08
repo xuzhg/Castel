4a44aa5291b2fcedde30d179d51c509e8fd1470f
Sam Xu <saxu@microsoft.com>
2018-10-08 12:00:07 -0700

    Add the projects

---
+++ samples/AspNetCoreODataSample.Web/Controllers/PeopleController.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using AspNetCoreODataSample.Web.Models;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Query;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreODataSample.Web.Controllers
+{
+    public class PeopleController : ODataController
+    {
+        [EnableQuery]
+        public IActionResult Get([FromODataUri]string keyFirstName, [FromODataUri]string keyLastName)
+        {
+            Person m = new Person
+            {
+                FirstName = keyFirstName,
+                LastName = keyLastName,
+                DynamicProperties = new Dictionary<string, object>
+                {
+                    { "abc", "abcValue" }
+                },
+                MyLevel = Level.High
+            };
+
+            return Ok(m);
+        }
+
+        [EnableQuery]
+        public IActionResult Post([FromBody]Person person)
+        {
+            return Created(person);
+        }
+    }
+
+    public class CustomersController : ODataController
+    {
+        //[EnableQuery]
+        public IActionResult Get(ODataQueryOptions<Customer> queryOptions)
+        {
+            Customer m = new Customer
+            {
+                Id = 1,
+                Name = "Ok"
+            };
+            IList<Customer> customers = new List<Customer> { m };
+            var queryable = customers.AsQueryable();
+            var query = queryOptions.ApplyTo(queryable, new ODataQuerySettings()) as IQueryable<Customer>;
+
+            var result = new List<Customer>();
+            foreach(var item in query)
+            {
+                item.OrdStat = "asf";
+                result.Add(item);
+            }
+
+            return Ok(result.AsQueryable());
+        }
+
+        [EnableQuery]
+        public IActionResult Post([FromBody]Customer person)
+        {
+            return Created(person);
+        }
+    }
+}
---
+++ samples/AspNetCoreODataSample.Web/Models/EdmModelBuilder.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.OData.Edm;
+using System.Collections.Generic;
+
+namespace AspNetCoreODataSample.Web.Models
+{
+    public static class EdmModelBuilder
+    {
+        private static IEdmModel _edmModel;
+
+        public static IEdmModel GetEdmModel()
+        {
+            if (_edmModel == null)
+            {
+                var builder = new ODataConventionModelBuilder();
+                builder.EntitySet<Movie>("Movies");
+                _edmModel = builder.GetEdmModel();
+            }
+
+            return _edmModel;
+        }
+
+        public static IEdmModel GetCompositeModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Person>("People");
+            var type = builder.EntitySet<Person>("Person").EntityType;
+            type.HasKey(x => new { x.FirstName, x.LastName });
+            return builder.GetEdmModel();
+        }
+
+        public static IEdmModel GetCustomerModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Customer>("Customers");
+            return builder.GetEdmModel();
+        }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public System.Int64 Date { get; set; }
+
+        public string OrdStat { get; set; }
+
+        public IDictionary<string, object> Dynamics { get; set; }
+    }
+}
